0635b967e60e1c2f36cd5ba94132d889b2401638
Hao Zhang <hzhangxyz@outlook.com>
2025-12-16 00:27:26 +0800

    Add output.py again.

--- main.py
+++ main.py
@@ -4,11 +4,17 @@ from orm import initialize_database
 from ds import main as ds
 from egg import main as egg
 from input import main as input
+from output import main as output
 
 
 async def main(addr):
     engine, session = await initialize_database(addr)
-    await asyncio.gather(ds(addr, engine, session), egg(addr, engine, session), input(addr, engine, session))
+    await asyncio.gather(
+        ds(addr, engine, session),
+        egg(addr, engine, session),
+        input(addr, engine, session),
+        output(addr, engine, session),
+    )
 
 
 if __name__ == "__main__":
---
+++ output.py
@@ -0,0 +1,38 @@
+import sys
+import asyncio
+from sqlalchemy import select
+from orm import initialize_database, Facts, Ideas
+from poly import Poly
+
+
+async def main(addr, engine=None, session=None):
+    if engine is None or session is None:
+        engine, session = await initialize_database(addr)
+
+    max_fact = -1
+    max_idea = -1
+
+    while True:
+        count = 0
+        begin = asyncio.get_running_loop().time()
+
+        async with session() as sess:
+            for i in await sess.scalars(select(Facts).where(Facts.id > max_fact)):
+                max_fact = max(max_fact, i.id)
+                print("fact:", Poly(dsp=i.data).dsp)
+            for i in await sess.scalars(select(Ideas).where(Ideas.id > max_idea)):
+                max_idea = max(max_idea, i.id)
+                print("idea:", Poly(dsp=i.data).dsp)
+
+        end = asyncio.get_running_loop().time()
+        duration = end - begin
+        if count == 0:
+            delay = max(0, 1 - duration)
+            await asyncio.sleep(delay)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print(f"Usage: {sys.argv[0]} <database-addr>")
+        sys.exit(1)
+    asyncio.run(main(sys.argv[1]))
