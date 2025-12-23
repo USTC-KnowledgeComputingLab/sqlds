6fabffdd95043f1be101310ffdd45e08de92b7fd
Hao Zhang <hzhangxyz@outlook.com>
2025-12-23 17:41:54 +0800

    Add a router for insert or ignore across different databases.

--- ddss/orm.py
+++ ddss/orm.py
@@ -5,6 +5,9 @@ from sqlalchemy.ext.asyncio import AsyncEngine, AsyncSession
 from sqlalchemy.exc import IntegrityError
 from sqlalchemy import Integer, Text
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy.dialects.sqlite import insert as sqlite_insert
+from sqlalchemy.dialects.mysql import insert as mysql_insert
+from sqlalchemy.dialects.postgresql import insert as postgresql_insert
 
 
 class Base(DeclarativeBase):
@@ -31,11 +34,22 @@ async def initialize_database(addr: str) -> tuple[AsyncEngine, async_sessionmake
     return engine, session
 
 
-async def insert_or_ignore(sess: AsyncSession, model, data, locks=defaultdict(lambda: asyncio.Lock())) -> None:
-    async with locks[id(sess.bind)]:
-        try:
-            async with sess.begin_nested():
-                sess.add(model(data=data))
-                await sess.flush()
-        except IntegrityError:
-            pass
+async def insert_or_ignore(sess: AsyncSession, model: type[Base], data: str, locks=defaultdict(asyncio.Lock)) -> None:
+    match sess.bind.dialect.name:
+        case "sqlite":
+            statement = sqlite_insert(model).values(data=data).on_conflict_do_nothing()
+            await sess.execute(statement)
+        case "mysql" | "mariadb":
+            statement = mysql_insert(model).values(data=data).prefix_with("IGNORE")
+            await sess.execute(statement)
+        case "postgresql":
+            statement = postgresql_insert(model).values(data=data).on_conflict_do_nothing()
+            await sess.execute(statement)
+        case _:
+            async with locks[id(sess.bind)]:
+                try:
+                    async with sess.begin_nested():
+                        sess.add(model(data=data))
+                        await sess.flush()
+                except IntegrityError:
+                    pass
