6c6720272fd358146d2b6b4f776f1664d11422d8
Hao Zhang <hzhangxyz@outlook.com>
2025-12-26 10:59:41 +0800

    Update ddss/dump.ts.

--- ddss/dump.ts
+++ ddss/dump.ts
@@ -1,23 +1,19 @@
-import { unparse } from "atsds-bnf";
-import { initializeDatabase, Fact, Idea } from "./orm.js";
 import type { Sequelize } from "sequelize";
+import { unparse } from "atsds-bnf";
+import { Fact, Idea, initializeDatabase } from "./orm.ts";
 
 export async function main(addr: string, sequelize?: Sequelize) {
     if (!sequelize) {
         sequelize = await initializeDatabase(addr);
     }
 
-    try {
-        const ideas = await Idea.findAll();
-        for (const idea of ideas) {
-            console.log("idea:", unparse(idea.data));
-        }
+    const ideas = await Idea.findAll();
+    for (const idea of ideas) {
+        console.log("idea:", unparse(idea.data));
+    }
 
-        const facts = await Fact.findAll();
-        for (const fact of facts) {
-            console.log("fact:", unparse(fact.data));
-        }
-    } finally {
-        // Session is handled by main or caller
+    const facts = await Fact.findAll();
+    for (const fact of facts) {
+        console.log("fact:", unparse(fact.data));
     }
 }
